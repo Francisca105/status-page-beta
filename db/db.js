f4c2dd3779334560b1142775be01e4c63ec0ebf0
Francisca105 <65908870+Francisca105@users.noreply.github.com>
2020-09-14 08:02:04 +0100

    Update db.js

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const { Sequelize } = require("sequelize")
 
-module.exports = new Sequelize(/*process.env.TABELA, process.env.USER, process.env.PASS, {*/ 's66_status', 'u66_Z9gYfTYsqN', 'by5L^+YlH@fAH!PEaoRPi1Gl', {
-    host: '51.81.121.137', //process.env.IP,
+module.exports = new Sequelize(process.env.TABELA, process.env.USER, process.env.PASS,{
+    host: process.env.IP,
     dialect: 'mysql'
 })
